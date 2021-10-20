299425f312be0beb3d80eccdfe646d2bedd8bae2
Manapat Weeramongkolkul <hungkeng99@gmail.com>
2021-10-20 16:29:03 +0700

    add socket

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const app = require("./app");
 const server = http.createServer(app);
+const io = require('socket.io')(http);
 
 const { API_PORT } = process.env;
 const port = process.env.PORT || API_PORT;
@@ -8,4 +9,23 @@ const port = process.env.PORT || API_PORT;
 // server listening 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+io.on("connection", function (socket) {
+  let setting = {
+    "digit": 5,
+    "round": 3,
+    "timeLimit": 90,
+    "isBasicMode": true,
+  } 
+  console.log("Connected!");
+  io.emit("updateSetting",setting)  
+
+  socket.on("updateSetting", function(setting){
+    console.log("Setting is being updated.");
+    io.emit("updateSetting",setting)
+  });
+  socket.on("disconnect", function(){
+    console.log("Someone left the game");
+  });
 });
