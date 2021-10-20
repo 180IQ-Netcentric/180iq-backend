eab79a2021bc086a090779abbb5dcf2a888bb184
Natthorn <tum210544@gmail.com>
2021-10-20 17:33:30 +0700

    Socket wip

--- index.js
+++ index.js
@@ -12,19 +12,19 @@ server.listen(port, () => {
 });
 
 io.on("connection", function (socket) {
-  let setting = {
-    "digit": 5,
-    "round": 3,
-    "timeLimit": 90,
-    "isBasicMode": true,
-  } 
+  // let setting = {
+  //   "digit": 5,
+  //   "round": 3,
+  //   "timeLimit": 90,
+  //   "isBasicMode": true,
+  // } 
   console.log("Connected!");
-  io.emit("updateSetting",setting)  
+  // io.emit("updateSetting",setting)  
 
-  socket.on("updateSetting", function(setting){
-    console.log("Setting is being updated.");
-    io.emit("updateSetting",setting)
-  });
+  // socket.on("updateSetting", function(setting){
+  //   console.log("Setting is being updated.");
+  //   io.emit("updateSetting",setting)
+  // });
   socket.on("disconnect", function(){
     console.log("Someone left the game");
   });
